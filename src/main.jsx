872f2503905aab57c0e8f454c866d5d5b9dc4b9d
RohitTiz <rohittiwari06052004@gmail.com>
2025-07-19 10:15:57 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
 import './App.css'
-
+//css
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
